27b3577bdeba02a3795781bc5920b289376d0a13
user741852 <wo.xiaoyao.zizai@163.com>
2020-12-02 16:01:00 +0800

    improve retring

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const roomid = myArgs[0];
 const danmuClient = new huya_danmu(roomid)
 const mongoClient = require('mongodb').MongoClient;
 
+var log4js = require("log4js");
+var logger = log4js.getLogger();
+logger.level = "info";
+log4js.configure({
+    appenders: { file: { type: "file", filename: defaultRoomId + "_output.log", maxLogSize: 10 * 1024 * 1024 }, console: { type: 'stdout' } },
+    categories: { default: { appenders: ["console", "file"], level: "info" } }
+});
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -12,60 +19,60 @@ const url = 'mongodb://localhost:27017';
 let mongoDbCLient = null;
 
 danmuClient.on('connect', () => {
-    console.log(`已连接huya ${roomid}房间弹幕~`);
+    logger.info(`已连接huya ${roomid}房间弹幕~`);
     // Use connect method to connect to the server
-    mongoClient.connect(url, function(err, dbClient) {
+    mongoClient.connect(url, function (err, dbClient) {
         if (err) throw err;
-        console.log("已连接到MongoDB");
+        logger.info("已连接到MongoDB");
         mongoDbCLient = dbClient;
     });
 })
 
 let huyaDB = null;
 danmuClient.on('message', msg => {
-    if(huyaDB == null){
+    if (huyaDB == null) {
         huyaDB = mongoDbCLient.db("huya");
     }
     switch (msg.type) {
         case 'chat':
-            //console.log(`[${msg.from.name}]:${msg.content}`)
-            huyaDB.collection(roomid).insertOne(msg, function(err, res) {
+            logger.debug(`[${msg.from.name}]:${msg.content}`)
+            huyaDB.collection(roomid).insertOne(msg, function (err, res) {
                 if (err) throw err;
             });
             break
         case 'gift':
-            //console.log(`[${msg.from.name}]->赠送${msg.count}个${msg.name}`)
-            huyaDB.collection(roomid).insertOne(msg, function(err, res) {
+            logger.debug(`[${msg.from.name}]->赠送${msg.count}个${msg.name}`)
+            huyaDB.collection(roomid).insertOne(msg, function (err, res) {
                 if (err) throw err;
             });
             break
         case 'userTV':
-            //console.log(`[${msg.from.name}]上电视了，内容是:${msg.content}`)
-            huyaDB.collection(roomid + "_tv").insertOne(msg,function(err, res){
+            logger.debug(`[${msg.from.name}]上电视了，内容是:${msg.content}`)
+            huyaDB.collection(roomid + "_tv").insertOne(msg, function (err, res) {
                 if (err) throw err;
             });
             break
         case 'tvAward':
-            //console.log(`上电视结束。中奖者是:${msg.from.name}，内容是${msg.content}`)
-            huyaDB.collection(roomid + "_tv").insertOne(msg,function(err, res){
+            logger.debug(`上电视结束。中奖者是:${msg.from.name}，内容是${msg.content}`)
+            huyaDB.collection(roomid + "_tv").insertOne(msg, function (err, res) {
                 if (err) throw err;
             });
             break
         case 'VipBarList':
-            huyaDB.collection(roomid + "_VipBarList").insertOne(msg,function(err, res){
+            logger.debug(msg)
+            huyaDB.collection(roomid + "_VipBarList").insertOne(msg, function (err, res) {
                 if (err) throw err;
             });
-            //console.log(msg)
             break
     }
 })
 
 danmuClient.on('error', e => {
-    console.log( (new Date()).toLocaleString() +e)
+    logger.info(e)
 })
 
 danmuClient.on('close', () => {
-    console.log((new Date()).toLocaleString()+':开始断线重连')
+    logger.info('开始断线重连')
     danmuClient.start()
 })
 
